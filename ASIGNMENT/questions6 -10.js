9ea165f6333330faf558f717405016a053688e58
Nwokolo victor oluebubechukwu {NEVO} <nwokoloebube21@gmail.com>
2025-12-31 14:49:57 +0100

    playing eith the, and used in separating strings

---
+++ ASIGNMENT/questions6 -10.js
@@ -0,0 +1,14 @@
+//assignment 6 to 10
+//6. Create a variable score with value 75. 
+// Write code that prints "Pass" if score is 50 or above, otherwise print "Fail". 
+let score=75;
+if(score>=50){
+    console.log("You passed");
+}else{console.log("you failed");}
+//7. Declare two variables a and b with values 12 and 15.
+//  Write code to check if a is greater than b and print an appropriate message. 
+let a=12;
+let b=15;
+if(a>b){
+    console.log(a,"is greater than",b);
+}else{console.log(b,"greater than",a);}
